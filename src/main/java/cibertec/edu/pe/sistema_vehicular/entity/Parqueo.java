51772c1c02ad774e54321d46d5f31b229f5c8aca
ZarelaMC <zarelamanrique87@gmail.com>
2024-10-24 11:12:39 -0500

    fix: Lógica parqueo y espacio parqueo

---
+++ src/main/java/cibertec/edu/pe/sistema_vehicular/entity/Parqueo.java
@@ -0,0 +1,23 @@
+package cibertec.edu.pe.sistema_vehicular.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "parqueo")
+public class Parqueo {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idParqueo;
+	
+    private String ubicacion;
+    private int cantEspaciosGenerales;
+    private int cantEspaciosDiscapacitados;
+    private int capacidadTotal;
+    private String tipoVehiculoPermitido;
+}
---
+++ src/main/java/cibertec/edu/pe/sistema_vehicular/service/EspacioParqueoService.java
@@ -0,0 +1,11 @@
+package cibertec.edu.pe.sistema_vehicular.service;
+
+import java.util.List;
+
+import cibertec.edu.pe.sistema_vehicular.entity.EspacioParqueo;
+
+public interface EspacioParqueoService {
+
+	List<EspacioParqueo> listarEspaciosParqueoTodos();
+	List<EspacioParqueo> listarEspaciosIdParqueo(int idParq);
+}
