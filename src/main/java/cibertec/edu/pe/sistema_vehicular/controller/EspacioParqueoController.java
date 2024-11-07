bfb13837286e1636bd54b9d16da76ba9e90ad27f
FatimaUrpi <fc200509fatima.chavez09@gmail.com>
2024-11-07 13:43:18 -0500

    Feat:Controller Espacio Parqueo modificación

--- src/main/java/cibertec/edu/pe/sistema_vehicular/controller/EspacioParqueoController.java
+++ src/main/java/cibertec/edu/pe/sistema_vehicular/controller/EspacioParqueoController.java
@@ -7,10 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cibertec.edu.pe.sistema_vehicular.entity.Cliente;
 import cibertec.edu.pe.sistema_vehicular.entity.EspacioParqueo;
 import cibertec.edu.pe.sistema_vehicular.service.EspacioParqueoService;
 import cibertec.edu.pe.sistema_vehicular.util.AppSettings;
@@ -40,4 +43,10 @@ public class EspacioParqueoController {
 			return ResponseEntity.ok(lista); //200 y la lista
 		}
 	}
+	
+	@PostMapping
+	public EspacioParqueo crearEspacioParqueo(@RequestBody EspacioParqueo espacioParqueo) {
+		return espacioService.crearEspacioParqueo(espacioParqueo);
+	}
+	
 }
